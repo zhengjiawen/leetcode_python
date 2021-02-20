677f5736beea703bde16521412a20f7e1caa619c
zhengjiawen <zhengjiawen77@gmail.com>
2021-02-20 19:11:28 +0800

    init

---
+++ linked_list/leetcode92.py
@@ -0,0 +1,21 @@
+'''
+反转从位置 m 到 n 的链表。请使用一趟扫描完成反转。
+
+说明:
+1 ≤ m ≤ n ≤ 链表长度。
+'''
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
+        pass
+
+
+if __name__ == '__main__':
+    solution = Solution()
